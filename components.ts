8f03b1f41d3eb75d95046e85c7cb3869d6ba29d9
SupaSibs <92636078+SupaSibs@users.noreply.github.com>
2022-02-16 13:43:37 +0200

    comp basic

--- components.ts
+++ components.ts
@@ -1,6 +1,17 @@
 
-export {
 class Component {
+    props: any;
+constructor(props: object) {
+this.props = props;
+}
+
+setState(fn) {
+fn();
+}
+renderComp(fn: function) {
 
 }
 }
+
+
+export {Component}
